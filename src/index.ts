99da827c1060afe7a4f329f4748a698ac5c1c7cc
nguye374 <nguye374@sheridancollege.ca>
2022-07-05 23:45:15 -0400

    add event class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
-user.fetch();
+const user = new User({ name: 'viet', age: 29 });
+// user.set({ name: 'new viet', age: 30 });
+user.save();
