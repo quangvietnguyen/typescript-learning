7c0c9d51d00a299eda79547c26b44f424dd999cf
nguye374 <nguye374@sheridancollege.ca>
2022-07-06 01:07:20 -0400

    refactor Sync class

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import { User } from './models/User';
 const user = new User({ name: 'viet', age: 29 });
 // user.set({ name: 'new viet', age: 30 });
 user.save();
+
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
