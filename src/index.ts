0b5b6163c2d52a0cce68dbbf3dfcc604f313383d
nguye374 <nguye374@sheridancollege.ca>
2022-07-07 02:03:24 -0400

    finish refactor Model class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'newer viet', age: 33 });
+const user = User.buildUser({ id: 1, name: 'newest viet', age: 33 });
 
 user.on('save', () => {
   console.log(user);
